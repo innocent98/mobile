6a01a122da159bd802cf32a2ca09fc3286a53516
Victor Oluwatosin <tosinadebayo55@gmail.com>
2023-08-30 01:01:41 +0000

    updated

--- babel.config.js
+++ babel.config.js
@@ -1,15 +1,4 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
-  plugins: [
-    'react-native-reanimated/plugin',
-    [
-      'module-resolver',
-      {
-        root: ['./'],
-        alias: {
-          'moti/skeleton': 'moti/skeleton/react-native-linear-gradient',
-        },
-      },
-    ],
-  ],
+  plugins: ['react-native-reanimated/plugin'],
 };
